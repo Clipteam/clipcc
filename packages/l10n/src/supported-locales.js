96f2a9641d3ac3f427c1e3884af435af40db4a58
frank <frank_782@qq.com>
2023-02-01 22:20:26 +0800

    :sparkles: migrate l10n

---
+++ packages/l10n/src/supported-locales.js
@@ -0,0 +1,47 @@
+/**
+ * Currently supported locales for the Scratch Project
+ * @type {Object} Key Value pairs of locale code: Language name written in the language
+ */
+
+const locales = {
+    'en': {name: 'English'},
+    'fr': {name: 'Français'},
+    'zh-cn-chunibyo': {name: '中二模式'},
+    'zh-cn': {name: '简体中文'},
+    'zh-tw': {name: '繁體中文'}
+};
+
+const customLocales = {
+    // TODO: replace zh-cn, zh-tw with zh-Hans and zh-Hant then customLocales is unnecessary
+    'zh-cn': {
+        locale: 'zh-cn',
+        parentLocale: 'zh'
+    },
+    'zh-tw': {
+        locale: 'zh-tw',
+        parentLocale: 'zh'
+    }
+};
+
+const localeMap = {
+    'aa-dj': 'aa_DJ',
+    'es-419': 'es_419',
+    // ja-Hira: no map - it's 'ja-Hira' on transifex
+    'pt-br': 'pt_BR',
+    'zh-cn': 'zh_CN',
+    'zh-tw': 'zh_TW'
+};
+
+// list of RTL locales supported, and a function to check whether a locale is RTL
+const rtlLocales = [
+    'ar',
+    'ckb',
+    'fa',
+    'he'
+];
+
+const isRtl = locale => {
+    return rtlLocales.indexOf(locale) !== -1;
+};
+
+export {locales as default, customLocales, localeMap, isRtl};
