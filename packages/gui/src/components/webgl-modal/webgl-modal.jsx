8ec1ba426103ecad45f420402aeff2a43d7ca6cf
Alex Cui <cuizhihui030925@outlook.com>
2023-02-03 17:24:46 +0800

    :pencil2: modify translations related to scratch
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/gui/src/components/webgl-modal/webgl-modal.jsx
+++ packages/gui/src/components/webgl-modal/webgl-modal.jsx
@@ -32,7 +32,7 @@ const WebGlModal = ({intl, ...props}) => (
                 <p>
                     { /* eslint-disable max-len */ }
                     <FormattedMessage
-                        defaultMessage="Unfortunately it looks like your browser or computer {webGlLink}. This technology is needed for Scratch 3.0 to run."
+                        defaultMessage="Unfortunately it looks like your browser or computer {webGlLink}. This technology is needed for ClipCC to run."
                         description="WebGL missing message"
                         id="gui.webglModal.description"
                         values={{
