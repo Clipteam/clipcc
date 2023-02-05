65675f1a67e2dd86f6844ef2ec5c988ffd465d50
SimonShiki <sinangentoo@gmail.com>
2023-02-05 13:48:01 +0000

    :sparkles: Add commitlint
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['gitmoji'],
+    rules: {
+        'type-empty': [2, 'always'],
+        'signed-off-by': [2, 'always', 'Signed-off-by:'],
+        'subject-empty': [2, 'always']
+    }
+};
