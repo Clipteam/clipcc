1d8826dea907a44f381ba1316325bc6b6818a69e
SimonShiki <sinangentoo@gmail.com>
2023-02-12 09:09:47 +0000

    :sparkles: upgrade to react 16.14
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/gui/src/.eslintrc.js
+++ packages/gui/src/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     },
     settings: {
         react: {
-            version: '16.2' // Prevent 16.3 lifecycle method errors
+            version: 'detect'
         },
         'import/resolver': {
             webpack: {
