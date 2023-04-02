c4dbee93fa98c994d8e5c2e965b56648e2202958
SimonShiki <sinangentoo@gmail.com>
2023-04-02 15:48:17 +0000

    :bug: fix(vm): missing comma in music extension tests
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/vm/test/unit/extension_music.js
+++ packages/vm/test/unit/extension_music.js
@@ -3,7 +3,7 @@ const Music = require('../../src/extensions/scratch3_music/index.js');
 
 const fakeRuntime = {
     getTargetForStage: () => ({tempo: 60}),
-    limitOptions: {}
+    limitOptions: {},
     on: () => {} // Stub out listener methods used in constructor.
 };
 
