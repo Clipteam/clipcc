6f79e0e3560cd33724ba52a9125a334a6512056d
SinanGentoo <sinangentoo@gmail.com>
2023-04-02 20:01:35 +0800

    :bug: fix(gui): vm-related settings not be initialized correctly
    Signed-off-by: SinanGentoo <sinangentoo@gmail.com>

--- packages/gui/src/containers/settings-modal.jsx
+++ packages/gui/src/containers/settings-modal.jsx
@@ -45,7 +45,6 @@ class SettingsModal extends React.Component {
         value = Math.round(value);
         if (value >= 10 && value <= 240) {
             this.props.updateSettings({framerate: value});
-            this.props.vm.setFramerate(value);
         }
     }
     handleChangeInfiniteCloning (value) {
