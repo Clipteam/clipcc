3210fc0d0a8761ef4cd8dcd8b0ab3cb7071b29f6
SimonShiki <sinangentoo@gmail.com>
2023-04-19 15:28:02 +0000

    :wrench: chore: add performance test
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

---
+++ packages/vm/test/extra/performance.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+const log = require('../../src/util/log');
+const makeTestStorage = require('../fixtures/make-test-storage');
+const readFileToBuffer = require('../fixtures/readProjectFile').readFileToBuffer;
+const VirtualMachine = require('../../src/index');
+
+/**
+* @fileoverview Test vm's performance.
+*/
+
+let averageTime = -1;
+
+const whenTaskComplete = () => {
+    // When the number of threads reaches 0 the test is expected to be complete.
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            if (averageTime != -1) resolve();
+        }, 50);
+    });
+};
+
+const reportVmResult = (text) => {
+    const separatorIndex = text.indexOf(' ');
+    const command = text.substring(0, separatorIndex);
+    const value = text.substring(separatorIndex + 1);
+    switch (command) {
+    case 'finished':
+        averageTime = value;
+        break;
+    case 'invaild':
+        throw new Error('invaild quicksort result: ' + value);
+        break;
+    default:
+        console.log(`${command} ${value}`);
+    }
+};
+
+const filePath = path.resolve(__dirname, '../fixtures/performance-test.sb3');
+const vm = new VirtualMachine();
+vm.attachStorage(makeTestStorage());
+
+// Start the VM and initialize some vm properties.
+// complete.
+vm.start();
+vm.clear();
+vm.setCompatibilityMode(false);
+vm.setTurboMode(false);
+
+// Report the text of SAY events as testing instructions.
+vm.runtime.on('SAY', (target, type, text) => reportVmResult(text));
+
+console.log('50k items qsort performance test\n');
+
+const project = readFileToBuffer(filePath);
+
+// Load the project and once all threads are complete ensure that
+// the scratch project sent us a "end" message.
+return vm.loadProject(project)
+.then(() => vm.greenFlag())
+.then(() => whenTaskComplete())
+.then(() => {
+    vm.quit();
+    console.log('test finished! average time is ' + averageTime);
+    process.exit(0);
+});
