0d05936b0c6469bf6c345bedc63fe019a3b32046
SimonShiki <sinangentoo@gmail.com>
2023-05-02 15:11:43 +0000

    :bug: fix(vm): purify object before transferring to worker
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/vm/src/dispatch/shared-dispatch.js
+++ packages/vm/src/dispatch/shared-dispatch.js
@@ -126,10 +126,7 @@ class SharedDispatch {
         return new Promise((resolve, reject) => {
             const responseId = this._storeCallbacks(resolve, reject);
 
-            /** @TODO: remove this hack! this is just here so we don't try to send `util` to a worker */
-            if ((args.length > 0) && (typeof args[args.length - 1].yield === 'function')) {
-                args.pop();
-            }
+            args = this._purifyObject(args);
 
             if (transfer) {
                 provider.postMessage({service, method, responseId, args}, transfer);
@@ -137,6 +134,41 @@ class SharedDispatch {
                 provider.postMessage({service, method, responseId, args});
             }
         });
+    }
+
+    /**
+     * Purify an object so that it can be safely transferred to the worker.
+     * @param {obj} object - The Object that need to be purified.
+     * @returns {object} - purified object.
+     */
+    _purifyObject (obj, visited = new Set(), depth = 1) {
+        // Unlimited depth recursion is very expensive to execute
+        // So we should limit the maximum recursion depth.
+        if (depth > 5) return undefined;
+
+        if (typeof obj === "function" || typeof obj === "symbol") {
+            return undefined;
+        }
+
+        if (obj !== null && typeof obj === "object") {
+            if (visited.has(obj)) return undefined;
+            visited.add(obj);
+
+            if (Array.isArray(obj)) {
+                return obj.map((item) => this._purifyObject(item, visited, depth + 1));
+            } else {
+                const result = {};
+                for (const key in obj) {
+                    const value = obj[key];
+                    const sanitizedValue = this._purifyObject(value, visited, depth + 1);
+                    if (sanitizedValue !== undefined) {
+                        result[key] = sanitizedValue;
+                    }
+                }
+                return result;
+            }
+        }
+        return obj;
     }
 
     /**
