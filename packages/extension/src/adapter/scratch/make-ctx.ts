7e6f71eff86ce27a9a4284fcb9ed8f19967c1f3d
SimonShiki <sinangentoo@gmail.com>
2023-05-11 14:21:32 +0000

    :wrench: chore(ext): add cast to ctx & sandboxed -> unsandboxed
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/extension/src/adapter/scratch/make-ctx.ts
+++ packages/extension/src/adapter/scratch/make-ctx.ts
@@ -6,17 +6,19 @@ import {
     StandardScratchExtensionClass as ExtensionClass
 } from '../../type/scratch';
 import { VM } from '../../type/virtual-machine';
+import { Cast } from '../../util';
 
 export interface Ctx {
-    ArgumentType: typeof ArgumentType,
-    BlockType: typeof BlockType,
-    TargetType: typeof TargetType,
-    ReporterScope: typeof ReporterScope,
+    ArgumentType: typeof ArgumentType
+    BlockType: typeof BlockType
+    TargetType: typeof TargetType
+    ReporterScope: typeof ReporterScope
+    Cast: Cast
     extensions: {
         register: (extensionObj: ExtensionClass) => void,
-        sandboxed: boolean
-    },
-    vm?: VM;
+        unsandboxed: boolean
+    }
+    vm?: VM
 }
 
 export function makeCtx (sandboxed = false) {
@@ -25,11 +27,12 @@ export function makeCtx (sandboxed = false) {
         BlockType: BlockType,
         TargetType: TargetType,
         ReporterScope: ReporterScope,
+        Cast: Cast,
         extensions: {
             register: () => {
                 throw new Error('not implemented');
             },
-            sandboxed: sandboxed
+            unsandboxed: !sandboxed
         }
     };
     if (!sandboxed) {
--- packages/extension/src/util/index.ts
+++ packages/extension/src/util/index.ts
@@ -1,1 +1,2 @@
 export * from './maybe-format-message';
+export * from './cast';
