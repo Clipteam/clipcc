f3b76dcd5e054b8677a5b892da18b5f86894b5e3
frank <frank_782@qq.com>
2023-05-13 00:16:57 +0800

    :sparkles: chore: update to react-intl 6
    Signed-off-by: frank <frank_782@qq.com>

--- packages/l10n/src/index.js
+++ packages/l10n/src/index.js
@@ -1,3 +1,2 @@
-import localeData from './locale-data.js';
 import locales, {localeMap, isRtl} from './supported-locales.js';
-export {locales as default, localeData, localeMap, isRtl};
+export {locales as default, localeMap, isRtl};
