a7b613f21da81774da50e5ebba1506d533205766
SimonShiki <sinangentoo@gmail.com>
2023-06-07 14:45:36 +0000

    Revert ":bug: fix(vm): incorrect ScratchStorage require in tests"
    This reverts commit 82d30a6fe4a238fb9c7792f62e0b99bac5aff326.

--- packages/vm/test/integration/offline-custom-assets.js
+++ packages/vm/test/integration/offline-custom-assets.js
@@ -8,7 +8,7 @@ const path = require('path');
 const fs = require('fs');
 const test = require('tap').test;
 const AdmZip = require('adm-zip');
-const { ScratchStorage } = require('clipcc-storage');
+const ScratchStorage = require('clipcc-storage');
 const VirtualMachine = require('../../src/index');
 
 const projectUri = path.resolve(__dirname, '../fixtures/offline-custom-assets.sb2');
