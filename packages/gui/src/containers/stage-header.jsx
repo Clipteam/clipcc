4d07bbd403520c433d5e5fc60eef947d393c49bc
frank <frank_782@qq.com>
2023-08-18 20:00:00 +0800

    :sparkles: feat(gui): seamless fullscreen
    Signed-off-by: frank <frank_782@qq.com>

--- packages/gui/src/containers/stage-header.jsx
+++ packages/gui/src/containers/stage-header.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import bindAll from 'lodash.bindall';
 import VM from 'clipcc-vm';
+import bowser from 'bowser';
 import {STAGE_SIZE_MODES} from '../lib/layout-constants';
 import {setStageSize} from '../reducers/stage-size';
 import {setFullScreen} from '../reducers/mode';
@@ -10,12 +11,14 @@ import {connect} from 'react-redux';
 
 import StageHeaderComponent from '../components/stage-header/stage-header.jsx';
 
+
 // eslint-disable-next-line react/prefer-stateless-function
 class StageHeader extends React.Component {
     constructor (props) {
         super(props);
         bindAll(this, [
-            'handleKeyPress'
+            'handleKeyPress',
+            'handleClickFullScreen'
         ]);
     }
     componentDidMount () {
@@ -24,9 +27,36 @@ class StageHeader extends React.Component {
     componentWillUnmount () {
         document.removeEventListener('keydown', this.handleKeyPress);
     }
+    toggleFullScreen (fullscreen) {
+        if (fullscreen && !document.fullscreenElement) {
+            document.documentElement.requestFullscreen()
+                .then(
+                    // Emit resize on window to get blocks to resize
+                    () => window.dispatchEvent(new Event('resize'))
+                );
+        } else if (!fullscreen && document.exitFullscreen && document.fullscreenElement) {
+            document.exitFullscreen()
+                .then(
+                    // Emit resize on window to get blocks to resize
+                    () => window.dispatchEvent(new Event('resize'))
+                );
+        }
+    }
     handleKeyPress (event) {
         if (event.key === 'Escape' && this.props.isFullScreen) {
             this.props.onSetStageUnFull(false);
+        }
+    }
+    handleClickFullScreen (event) {
+        const ctrlKey = bowser.mac ? event.metaKey : event.ctrlKey;
+        if (this.props.isFullScreen) {
+            this.toggleFullScreen(false);
+            this.props.onSetStageUnFull();
+        } else {
+            if (ctrlKey) {
+                this.toggleFullScreen(true);
+            }
+            this.props.onSetStageFull();
         }
     }
     render () {
@@ -36,6 +66,8 @@ class StageHeader extends React.Component {
         return (
             <StageHeaderComponent
                 {...props}
+                onSetStageFull={this.handleClickFullScreen}
+                onSetStageUnFull={this.handleClickFullScreen}
                 onKeyPress={this.handleKeyPress}
             />
         );
@@ -45,6 +77,7 @@ class StageHeader extends React.Component {
 StageHeader.propTypes = {
     isFullScreen: PropTypes.bool,
     isPlayerOnly: PropTypes.bool,
+    onSetStageFull: PropTypes.func.isRequired,
     onSetStageUnFull: PropTypes.func.isRequired,
     showBranding: PropTypes.bool,
     stageSizeMode: PropTypes.oneOf(Object.keys(STAGE_SIZE_MODES)),
