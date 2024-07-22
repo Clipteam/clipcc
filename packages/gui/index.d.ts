ae6c3a528a41cb3e1f391918410fd04c70e62661
SimonShiki <sinangentoo@gmail.com>
2024-07-22 21:37:04 +0800

    :wrench: chore(gui): add type definition
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

---
+++ packages/gui/index.d.ts
@@ -0,0 +1,49 @@
+import { Component } from "react";
+
+
+// Copied from clip-frontend-resurrection
+interface SelfInfo {
+    id: number;
+    name: string;
+    has_password: boolean;
+    has_checkin: boolean;
+    phone: string;
+    email: string;
+    avatar: string;
+    bio: string;
+    budge_type: number;
+    budge_text: string;
+    created_at: string;
+    updated_at: string;
+};
+
+export interface WrappedGUIProps {
+    id: string;
+    authorId?: number;
+    authorUsername?: string;
+    authorThumbnailUrl?: string;
+    backpackHost?: string;
+    backpackVisible?: boolean;
+    basePath: string;
+    projectId?: number;
+    projectTitle?: string;
+    projectHost: string;
+    canUseCloud?: boolean;
+    hasCloudPermission?: boolean;
+    cloudHost: string;
+    assetHost: string;
+    cdnHost: string;
+    isShared?: boolean;
+    canEditTitle?: boolean;
+    canRemix?: boolean;
+    canSave?: boolean;
+    canShare?: boolean;
+    canCreateNew?: boolean;
+    userInfo?: SelfInfo;
+    userToken?: string;
+    enableCommunity?: boolean;
+}
+
+declare class WrappedGUI extends Component<WrappedGUIProps> {}
+
+export = WrappedGUI;
