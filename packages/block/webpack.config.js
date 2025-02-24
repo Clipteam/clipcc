0bc16e88f29ae021ee59ab2ef8105d831acc16a9
Alex Cui <cuizhihui030925@outlook.com>
2025-02-25 00:31:35 +0800

    :tada: refactor(block): init modern blockly
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/webpack.config.js
+++ packages/block/webpack.config.js
@@ -1,99 +1,41 @@
-// patch 'fs' to fix EMFILE errors, for example on WSL
-var realFs = require('fs');
-var gracefulFs = require('graceful-fs');
-gracefulFs.gracefulify(realFs);
-
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var path = require('path');
-var TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
 
 module.exports = [{
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: {
-    // horizontal: './shim/horizontal.js',
-    vertical: './shim/vertical.js'
-  },
+  entry: './src/index.ts',
   output: {
     library: 'ScratchBlocks',
     libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-  optimization: {
-    minimize: false
+  resolve: {
+    extensions: ['.ts', '.js']
   },
-  performance: {
-    hints: false
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
   }
 }, {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: {
-    // horizontal: './shim/horizontal.js',
-    vertical: './shim/vertical.js'
-  },
+  entry: './src/index.ts',
   output: {
-    library: 'Blockly',
+    library: 'ScratchBlocks',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist', 'web'),
     filename: '[name].js'
   },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false
-        }
-      })
-    ]
+  resolve: {
+    extensions: ['.ts', '.js']
   },
-  plugins: []
-},
-{
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './shim/gh-pages.js',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'gh-pages')
-  },
-  optimization: {
-    minimize: false
-  },
-  performance: {
-    hints: false
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [{
-        from: 'node_modules/google-closure-library',
-        to: 'closure-library'
-      }, {
-        from: 'blocks_common',
-        to: 'playgrounds/blocks_common',
-      }, /*{
-        from: 'blocks_horizontal',
-        to: 'playgrounds/blocks_horizontal',
-      }, */{
-        from: 'blocks_vertical',
-        to: 'playgrounds/blocks_vertical',
-      }, {
-        from: 'core',
-        to: 'playgrounds/core'
-      }, {
-        from: 'media',
-        to: 'playgrounds/media'
-      }, {
-        from: 'msg',
-        to: 'playgrounds/msg'
-      }, {
-        from: 'tests',
-        to: 'playgrounds/tests'
-      }, {
-        from: '*.js',
-        to: 'playgrounds',
-        globOptions: {
-          ignore: 'webpack.config.js'
-        }
-      }]
-    })
-  ]
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  }
 }];
