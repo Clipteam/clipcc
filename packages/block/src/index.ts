9e5d2f453a22165f6dc337e3ee4082133017c6dd
Alex Cui <cuizhihui030925@outlook.com>
2025-02-26 00:27:03 +0800

    :rotating_light: chore: fix linting errors
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/src/index.ts
+++ packages/block/src/index.ts
@@ -1,6 +1,6 @@
 import * as Blockly from 'blockly';
 
 export function inject(container: Element | string, options?: Blockly.BlocklyOptions) {
-    const workspace = Blockly.inject(container, options);
-    return workspace;
+  const workspace = Blockly.inject(container, options);
+  return workspace;
 }
