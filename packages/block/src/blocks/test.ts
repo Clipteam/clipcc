634e8fae1ca8f0ef0f2fa860a77f8d6e2d7d8f3b
Alex Cui <cuizhihui030925@outlook.com>
2025-02-26 00:58:05 +0800

    :sparkles: feat(block): test block of modern blockly
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

---
+++ packages/block/src/blocks/test.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2025 Clip Team
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as Blockly from 'blockly';
+
+Blockly.Blocks['test_add'] = {
+  init: function(this: Blockly.Block) {
+    this.jsonInit({
+      message0: 'add %1 %2',
+      args0: [{
+        type: 'input_value',
+        name: 'NUM1'
+      }, {
+        type: 'input_value',
+        name: 'NUM2'
+      }],
+      output: 'Number',
+      extensions: []
+    });
+  }
+};
--- packages/block/src/index.ts
+++ packages/block/src/index.ts
@@ -1,6 +1,18 @@
+/**
+ * @license
+ * Copyright 2025 Clip Team
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import * as Blockly from 'blockly';
+import './blocks/test';
 
 export function inject(container: Element | string, options?: Blockly.BlocklyOptions) {
+  const defaultOptions: Blockly.BlocklyOptions = {
+    renderer: 'zelos'
+  };
+  Object.assign(options ?? {}, defaultOptions);
+
   const workspace = Blockly.inject(container, options);
   return workspace;
 }
