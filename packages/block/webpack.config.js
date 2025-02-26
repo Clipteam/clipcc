ec6658169ccc5591be215c87a3a2ef6d7b0b72ba
Alex Cui <cuizhihui030925@outlook.com>
2025-02-26 21:33:56 +0800

    :wrench: chore(block): support webpack-dev-server
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/webpack.config.js
+++ packages/block/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
+const defaultsDeep = require('lodash.defaultsdeep');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = [{
+const baseConfig = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.ts',
   output: {
     library: 'ScratchBlocks',
     libraryTarget: 'commonjs2',
-    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
   resolve: {
@@ -23,27 +24,44 @@ module.exports = [{
       exclude: /node_modules/
     }]
   }
-}, {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './src/index.ts',
-  output: {
-    library: 'ScratchBlocks',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist', 'web'),
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: 'raw-loader',
-      include: path.resolve(__dirname, 'src')
-    }, {
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: /node_modules/
-    }]
-  }
-}];
+};
+
+module.exports = [
+  defaultsDeep({}, baseConfig, {
+    output: {
+      path: path.resolve(__dirname, 'dist')
+    }
+  }),
+  defaultsDeep({}, baseConfig, {
+    output: {
+      path: path.resolve(__dirname, 'dist', 'web')
+    }
+  }),
+  defaultsDeep({}, baseConfig, {
+    devServer: {
+      static: false,
+      host: '0.0.0.0',
+      port: process.env.PORT || 8071
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist', 'playground')
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{
+          from: path.resolve(require.resolve('blockly'), '../media'),
+          to: 'media'
+        }, {
+          from: 'media',
+          to: 'media'
+        }, {
+          from: 'dist',
+          to: 'dist'
+        }, {
+          from: 'tests/playground.html',
+          to: 'index.html'
+        }]
+      })
+    ]
+  })
+];
