3901db2d1e0ee33277f050650aad5dd7092946a7
Alex Cui <cuizhihui030925@outlook.com>
2025-03-03 17:12:49 +0800

    :wrench: chore(block): update eslint config with typescript
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

---
+++ packages/block/eslint.config.mjs
@@ -0,0 +1,80 @@
+import googleStyle from 'eslint-config-google';
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+import jsdoc from 'eslint-plugin-jsdoc';
+import globals from 'globals';
+
+// These rules are no longer supported, but the Google style package we depend
+// on hasn't been updated in years to remove them, even though they have been
+// removed from the repo. Manually delete them here to avoid breaking linting.
+delete googleStyle.rules['valid-jsdoc'];
+delete googleStyle.rules['require-jsdoc'];
+
+export default [
+  {
+    // Note: there should be no other properties in this object
+    ignores: [
+      // node modules
+      'node_modules/*',
+      // build artifacts
+      'msg/*',
+      'i18n/*',
+      'shim/*',
+      'dist/*',
+      'gh-pages/*',
+      'build/*',
+      // tests
+      'tests/*',
+      // old sources
+      'core/*',
+      'blocks_*/*'
+    ]
+  },
+  eslint.configs.recommended,
+  jsdoc.configs['flat/recommended'],
+  googleStyle,
+  {
+    rules: {
+      'max-len': ['error', {
+        code: 120,
+        tabWidth: 2,
+        ignoreUrls: true
+      }],
+      'no-use-before-define': ['error', {
+        classes: false
+      }],
+      'quote-props': ['warn', 'as-needed'],
+      'comma-dangle': ['error', 'never'],
+      'indent': ['error', 2, {
+        SwitchCase: 1
+      }],
+      // Allow any text in the license tag. Other checks are not relevant.
+      'jsdoc/check-values': ['off']
+    }
+  },
+  {
+    files: ['webpack.config.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+  ...tslint.config({
+    files: ['src/**/*.ts'],
+    extends: [
+      tslint.configs.recommended,
+      jsdoc.configs['flat/recommended-typescript']
+    ],
+    languageOptions: {
+      parser: tslint.parser
+    },
+    rules: {
+      'jsdoc/check-values': ['off'],
+      '@typescript-eslint/no-unused-vars': ['error', {
+        args: 'none'
+      }]
+    }
+  })
+];
