d79fe3799280eba7728d01ec50de167f09e0e926
Alex Cui <cuizhihui030925@outlook.com>
2025-03-04 20:10:35 +0800

    :bug: fix(block): fix locale error
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/src/index.ts
+++ packages/block/src/index.ts
@@ -5,6 +5,7 @@
  */
 
 import * as Blockly from 'blockly/core';
+
 import {createTheme} from './colours';
 import {registerFieldAngle} from './fields/angle';
 import {registerFieldButton} from './fields/button';
@@ -14,6 +15,8 @@ import {registerFieldNote} from './fields/note';
 import {registerScratchCategory} from './toolbox/category';
 import {ContinuousToolBox} from './toolbox/toolbox';
 import {ContinuousVerticalFlyout} from './toolbox/flyout';
+
+import * as en from 'blockly/msg/en';
 
 import './blocks/extensions';
 import './blocks/common';
@@ -39,6 +42,7 @@ export function inject(container: Element | string, options?: Blockly.BlocklyOpt
   };
   options = Object.assign(defaultOptions, options);
 
+  Blockly.setLocale(en);
   const workspace = Blockly.inject(container, options);
 
   return workspace;
