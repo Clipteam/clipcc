4fd663a621b355ced7f9d5384a5c1cadf69e280b
Alex Cui <cuizhihui030925@outlook.com>
2025-03-05 14:31:18 +0800

    :fire: chore(block): remove shims
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/shim/blockly_compressed_vertical-blocks_compressed.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('imports-loader?type=commonjs&imports=single|./shim/blockly_compressed_vertical.Blockly|Blockly!exports-loader?type=commonjs&exports=single|Blockly!../blocks_compressed');
