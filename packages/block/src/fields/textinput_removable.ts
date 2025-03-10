a9b8525ab4f10a08075705d38062d4e3a5e3a240
Alex Cui <cuizhihui030925@outlook.com>
2025-03-10 08:46:20 +0800

    :recycle: refactor(block): migrate FieldTextInputRemovable
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/src/fields/textinput_removable.ts
+++ packages/block/src/fields/textinput_removable.ts
@@ -22,88 +22,96 @@
  * @fileoverview Text input field with floating "remove" button.
  * @author pkaplan@media.mit.edu (Paul Kaplan)
  */
-'use strict';
 
-import * as goog from 'google-closure-library/closure/goog/goog.js';
-goog.declareModuleId('Blockly.FieldTextInputRemovable');
-
-import * as browserEvents from './browser_events';
-import * as common from './common';
-import {Field} from './field';
-import {FieldTextInput} from './field_textinput';
-import * as utils from './utils';
-import {WidgetDiv} from './widgetdiv';
-
-const dom = goog.require('goog.dom');
-const TagName = goog.require('goog.dom.TagName');
-
+import * as Blockly from 'blockly/core';
+import {ProcedureDeclarationBlock} from '../blocks/procedures';
 
 /**
  * Class for an editable text field displaying a deletion icon when selected.
- * @param {string} text The initial content of the field.
- * @param {Function=} opt_validator An optional function that is called
- *     to validate any constraints on what the user entered.  Takes the new
- *     text as an argument and returns either the accepted text, a replacement
- *     text, or null to abort the change.
- * @param {RegExp=} opt_restrictor An optional regular expression to restrict
- *     typed text to. Text that doesn't match the restrictor will never show
- *     in the text field.
- * @extends {FieldTextInput}
- * @constructor
  */
-export const FieldTextInputRemovable = function(text, opt_validator, opt_restrictor) {
-  FieldTextInputRemovable.superClass_.constructor.call(this, text,
-      opt_validator, opt_restrictor);
-};
-goog.inherits(FieldTextInputRemovable, FieldTextInput);
+class FieldTextInputRemovable extends Blockly.FieldTextInput {
+  private removeButtonMouseWrapper: Blockly.browserEvents.Data | null = null;
 
-/**
- * Show the inline free-text editor on top of the text with the remove button.
- * @private
- */
-FieldTextInputRemovable.prototype.showEditor_ = function() {
-  FieldTextInputRemovable.superClass_.showEditor_.call(this);
-
-  const div = WidgetDiv.DIV;
-  div.className += ' removableTextInput';
-  const removeButton =
-      dom.createDom(TagName.IMG, 'blocklyTextRemoveIcon');
-  removeButton.setAttribute('src',
-      common.getMainWorkspace().options.pathToMedia + 'icons/remove.svg');
-  this.removeButtonMouseWrapper_ = browserEvents.bind(removeButton,
-      'mousedown', this, this.removeCallback_);
-  div.appendChild(removeButton);
-};
-
-/**
- * Function to call when remove button is called. Checks for removeFieldCallback
- * on sourceBlock and calls it if possible.
- * @private
- */
-FieldTextInputRemovable.prototype.removeCallback_ = function() {
-  if (this.sourceBlock_ && this.sourceBlock_.removeFieldCallback) {
-    this.sourceBlock_.removeFieldCallback(this);
-  } else {
-    console.warn('Expected a source block with removeFieldCallback');
+  /**
+   * @param value The initial content of the field.
+   * @param validator An optional function that is called
+   *     to validate any constraints on what the user entered.  Takes the new
+   *     text as an argument and returns either the accepted text, a replacement
+   *     text, or null to abort the change.
+   * @param config A map of options used to configure the field.
+   */
+  constructor(
+    value?: string | typeof Blockly.Field.SKIP_SETUP,
+    validator?: Blockly.FieldTextInputValidator | null,
+    config?: Blockly.FieldTextInputConfig
+  ) {
+    super(value, validator, config);
   }
-};
+
+  /**
+   * Helper function to construct a FieldTextInputRemovable from a JSON arg object,
+   * dereferencing any string table references.
+   * @param options A JSON object with options (text, class, and spellcheck).
+   * @returns The new text input.
+   */
+  static override fromJson(options: Blockly.FieldTextInputFromJsonConfig): Blockly.FieldTextInput {
+    const text = Blockly.utils.parsing.replaceMessageReferences(options.text);
+    const field = new FieldTextInputRemovable(text, null, options);
+    if (typeof options.spellcheck == 'boolean') {
+      field.setSpellcheck(options.spellcheck);
+    }
+    return field;
+  }
+
+  override dispose(): void {
+    if (this.removeButtonMouseWrapper) {
+      Blockly.browserEvents.unbind(this.removeButtonMouseWrapper);
+    }
+  }
+
+  /**
+   * Show the inline free-text editor on top of the text with the remove button.
+   * @param event Optional mouse event that triggered the field to open, or
+   *     undefined if triggered programmatically.
+   * @param quietInput True if editor should be created without focus.
+   *     Defaults to false.
+   */
+  protected override showEditor_(event?: Event, quietInput?: boolean): void {
+    super.showEditor_(event, quietInput);
+    const div = Blockly.WidgetDiv.getDiv();
+    if (!div) {
+      return;
+    }
+    div.className += ' removableTextInput';
+    const removeButton = document.createElement('img');
+    removeButton.className = 'blocklyTextRemoveIcon';
+    removeButton.setAttribute(
+      'src',
+      Blockly.getMainWorkspace().options.pathToMedia + 'icons/remove.svg'
+    );
+    this.removeButtonMouseWrapper = Blockly.browserEvents.bind(
+      removeButton, 'mousedown', this, this.removeCallback
+    );
+    div.appendChild(removeButton);
+  }
+
+  /**
+   * Function to call when remove button is called. Checks for removeFieldCallback
+   * on sourceBlock and calls it if possible.
+   */
+  private removeCallback() {
+    const sourceBlock = this.getSourceBlock() as ProcedureDeclarationBlock | null;
+    if (sourceBlock && 'removeFieldCallback' in sourceBlock) {
+      sourceBlock.removeFieldCallback(this);
+    } else {
+      console.warn('Expected a source block with removeFieldCallback');
+    }
+  }
+}
 
 /**
- * Helper function to construct a FieldTextInputRemovable from a JSON arg object,
- * dereferencing any string table references.
- * @param {!Object} options A JSON object with options (text, class, and
- *                          spellcheck).
- * @returns {!FieldTextInputRemovable} The new text input.
- * @public
+ * Register the field and any dependencies.
  */
-FieldTextInputRemovable.fromJson = function(options) {
-  const text = utils.replaceMessageReferences(options['text']);
-  const field = new FieldTextInputRemovable(text, options['class']);
-  if (typeof options['spellcheck'] == 'boolean') {
-    field.setSpellcheck(options['spellcheck']);
-  }
-  return field;
-};
-
-Field.register(
-    'field_input_removable', FieldTextInputRemovable);
+export function registerFieldTextInputRemovable() {
+  Blockly.fieldRegistry.register('field_input_removable', FieldTextInputRemovable);
+}
