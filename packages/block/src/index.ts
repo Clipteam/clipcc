0e5b3179eb0138832735d9434da684c8e17529e7
Sparrow He <sparrowhe@gmail.com>
2025-10-22 20:23:53 +0800

    :recycle: refactor(block): migrate sensing_category
    Signed-off-by: Sparrow He <sparrowhe@gmail.com>

--- packages/block/src/index.ts
+++ packages/block/src/index.ts
@@ -28,6 +28,7 @@ import './blocks/looks';
 import './blocks/sound';
 import './blocks/operators';
 import './blocks/control';
+import './blocks/sensing';
 import './blocks/test';
 
 export function inject(container: Element | string, options?: Blockly.BlocklyOptions) {
