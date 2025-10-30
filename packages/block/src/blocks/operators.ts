da91fe64371f13de84f56abd20ef10b46ca55cf4
Alex Cui <cuizhihui030925@outlook.com>
2025-10-30 23:42:16 +0800

    :sparkles: feat(block): implement operator_join_multiple
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/src/blocks/operators.ts
+++ packages/block/src/blocks/operators.ts
@@ -20,6 +20,8 @@
 
 
 import * as Blockly from 'blockly/core';
+import {FieldButton} from '../fields/button';
+import {getWorkspaceOptionsFromBlock} from '../utils';
 
 /**
  * Block for adding two numbers.
@@ -286,257 +288,191 @@ Blockly.Blocks['operator_join'] = {
   }
 };
 
-// Blockly.Blocks['operator_join_multiple'] = {
-//   /**
-//    * Block for string join operator with multiple arguments.
-//    * @this Blockly.Block
-//    */
-//   init: function(this: Blockly.Block) {
-//     this.jsonInit({
-//       message0: Blockly.Msg.OPERATORS_JOIN_MULTIPLE,
-//       extensions: ['colours_operators', 'output_string']
-//     });
-//     this.argumentIds_ = [];
-//     this.plusminus_ = new Blockly.FieldPlusMinus(
-//         this.handlePlus_.bind(this),
-//         this.handleMinus_.bind(this),
-//         true,
-//         false
-//     );
-//     this.appendDummyInput('DUMMY_INPUT').appendField(this.plusminus_, 'PLUS_MINUS');
-//   },
-//   /**
-//    * Create XML to represent the arguments of a join block with multiple arguments.
-//    * @return {!Element} XML storage element.
-//    * @this Blockly.Block
-//    */
-//   mutationToDom: function() {
-//     const container = document.createElement('mutation');
-//     container.setAttribute('argumentids', JSON.stringify(this.argumentIds_));
-//     return container;
-//   },
-//   /**
-//    * Parse XML to restore the arguments of a join block with multiple arguments.
-//    * @param {!Element} xmlElement XML storage element.
-//    * @this Blockly.Block
-//    */
-//   domToMutation: function(xmlElement) {
-//     const argumentIds = xmlElement.getAttribute('argumentids');
-//     // don't update if args are not changed
-//     if (JSON.stringify(this.argumentIds_) === argumentIds) {
-//       return;
-//     }
-//     this.argumentIds_ = JSON.parse(argumentIds);
-//     // at least two inputs should exist.
-//     this.plusminus_.setEnableMinus(this.argumentIds_.length >= 3);
-//     this.updateDisplay_();
-//   },
-//   /**
-//    * Add context menu option to insert or delete an input.
-//    * @param {!Array} menuOptions List of menu options to add to.
-//    * @param {Blockly.Block} triggeredBlock The block that triggered the menu event.
-//    * @this Blockly.Block
-//    */
-//   customContextMenu: function(menuOptions, triggeredBlock) {
-//     if (triggeredBlock && triggeredBlock.isShadow()) {
-//       const index = this.findBlockIndex_(triggeredBlock) + 1;
-//       menuOptions.push({
-//         enabled: true,
-//         text: Blockly.Msg.INSERT_INPUT,
-//         callback: this.insertInputWithIndex_.bind(this, index, null)
-//       });
-//       menuOptions.push({
-//         enabled: this.argumentIds_.length >= 3,
-//         text: Blockly.Msg.DELETE_INPUT,
-//         callback: this.removeInputWithIndex_.bind(this, index, null)
-//       });
-//     }
-//   },
-//   /**
-//    * Find the index of the input in child blocks.
-//    * @param {Blockly.Block} block Block that needs to be found.
-//    * @return {number} The index, -1 if not found.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   findBlockIndex_: function(block) {
-//     const childBlocks = this.getChildren(true);
-//     for (let i = 0; i < childBlocks.length; ++i) {
-//       if (childBlocks[i] === block) {
-//         return i;
-//       }
-//     }
-//     return -1;
-//   },
-//   /**
-//    * The function that executed when plus button is pressed.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   handlePlus_: function() {
-//     this.insertInputWithIndex_(this.argumentIds_.length + 1);
-//   },
-//   /**
-//    * The function that executed when minus button is pressed.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   handleMinus_: function() {
-//     this.removeInputWithIndex_(this.argumentIds_.length);
-//   },
-//   /**
-//    * Insert a new input to a given index.
-//    * @param {number} index Index of new input.
-//    * @param {string} name Name of the new input.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   insertInputWithIndex_: function(index, name) {
-//     Blockly.Events.setGroup(true);
-//     const oldMutation = Blockly.Xml.domToText(this.mutationToDom());
-//     if (this.argumentIds_.length === 2) {
-//       this.plusminus_.setEnableMinus(true);
-//     }
+interface OperatorJoinMultipleExtraState {
+  argumentids: string[];
+}
 
-//     if (!name) name = Blockly.utils.genUid();
-//     this.argumentIds_.splice(index - 1, 0, name);
-//     const input = this.insertValueInput(index, name);
-//     Blockly.Events.disable();
-//     const newBlock = this.workspace.newBlock('text');
-//     newBlock.setFieldValue('', 'TEXT');
-//     newBlock.setShadow(true);
-//     if (this.rendered && !this.isInsertionMarker()) {
-//       newBlock.initSvg();
-//       newBlock.render(false);
-//     }
-//     Blockly.Events.enable();
-//     if (Blockly.Events.isEnabled()) {
-//       Blockly.Events.fire(new Blockly.Events.BlockCreate(newBlock));
-//     }
-//     newBlock.outputConnection.connect(input.connection);
+interface OperatorJoinMultipleBlock extends Blockly.BlockSvg {
+  argumentids: string[];
+  buttonMinus: FieldButton;
+  buttonPlus: FieldButton;
 
-//     const newMutation = Blockly.Xml.domToText(this.mutationToDom());
-//     Blockly.Events.fire(new Blockly.Events.BlockChange(this, 'mutation', null, oldMutation, newMutation));
-//     Blockly.Events.setGroup(false);
-//   },
-//   /**
-//    * Remove an input to a given index.
-//    * @param {number} index Index of input.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   removeInputWithIndex_: function(index) {
-//     // not allowed to remove input when there are less than 2 inputs
-//     if (this.argumentIds_.length <= 2) return;
-//     Blockly.Events.setGroup(true);
-//     const oldMutation = Blockly.Xml.domToText(this.mutationToDom());
-//     if (this.argumentIds_.length === 3) {
-//       this.plusminus_.setEnableMinus(false);
-//     }
-//     this.removeInput(this.argumentIds_[index - 1]);
-//     this.argumentIds_.splice(index - 1, 1);
-//     const newMutation = Blockly.Xml.domToText(this.mutationToDom());
-//     Blockly.Events.fire(new Blockly.Events.BlockChange(this, 'mutation', null, oldMutation, newMutation));
-//     Blockly.Events.setGroup(false);
-//   },
-//   /**
-//    * Update the block's structure and appearance to match the internally stored
-//    * mutation.
-//    * @private
-//    * @this Blockly.Block
-//    */
-//   updateDisplay_: function() {
-//     const wasRendered = this.rendered;
-//     this.rendered = false;
+  // Overrides
+  loadExtraState(state: OperatorJoinMultipleExtraState): void;
+  saveExtraState(): OperatorJoinMultipleExtraState;
 
-//     // disconnect old blocks, except the first one and the last one
-//     const connectionMap = {};
-//     for (let i = 1; i < this.inputList.length - 1; ++i) {
-//       const input = this.inputList[i];
-//       if (input.connection) {
-//         const target = input.connection.targetBlock();
-//         const saveInfo = {
-//           shadow: input.connection.getShadowDom(),
-//           block: target
-//         };
-//         connectionMap[input.name] = saveInfo;
-//         input.connection.setShadowDom(null);
-//         if (target) {
-//           input.connection.disconnect();
-//         }
-//       }
-//     }
+  onButtonMinusClick(): void;
+  onButtonPlusClick(): void;
+  updateDisplay(): void;
+}
 
-//     // remove all inputs, except the first one and the last one
-//     for (let i = 1; i < this.inputList.length - 1; ++i) {
-//       this.inputList[i].dispose();
-//     }
-//     this.inputList.splice(1, this.inputList.length - 2);
+/**
+ * Block for string join operator with multiple arguments.
+ */
+Blockly.Blocks['operator_join_multiple'] = {
+  init: function() {
+    this.jsonInit({
+      message0: Blockly.Msg.OPERATORS_JOIN_MULTIPLE,
+      extensions: ['colours_operators', 'output_string']
+    });
+    this.argumentids = [];
 
-//     // create inputs
-//     for (let i = 0; i < this.argumentIds_.length; ++i) {
-//       const id = this.argumentIds_[i];
-//       const input = this.insertValueInput(i + 1, id);
+    // Create buttons.
+    this.buttonPlus = new FieldButton(
+      getWorkspaceOptionsFromBlock(this).pathToMedia + 'icons/plus.svg',
+      this.onButtonPlusClick.bind(this)
+    );
+    this.buttonMinus = new FieldButton(
+      getWorkspaceOptionsFromBlock(this).pathToMedia + 'icons/minus.svg',
+      this.onButtonMinusClick.bind(this)
+    );
+    this.appendDummyInput('DUMMY_INPUT')
+      .appendField(this.buttonPlus, 'BUTTON_PLUS')
+      .appendField(this.buttonMinus, 'BUTTON_MINUS');
+  },
+  mutationToDom: function(): Element {
+    const container = document.createElement('mutation');
+    container.setAttribute('argumentids', JSON.stringify(this.argumentids));
+    return container;
+  },
+  domToMutation: function(xmlElement: Element) {
+    this.loadExtraState({
+      argumentids: JSON.parse(xmlElement.getAttribute('argumentids')!)
+    });
+  },
+  saveExtraState: function(): OperatorJoinMultipleExtraState {
+    return {
+      argumentids: this.argumentids
+    };
+  },
+  loadExtraState: function(state: OperatorJoinMultipleExtraState) {
+    this.argumentids = state.argumentids;
+    this.updateDisplay();
+  },
+  /**
+   * Click event handler for plus button.
+   */
+  onButtonPlusClick: function() {
+    const oldExtraState = this.saveExtraState();
+    this.argumentids.push(Blockly.utils.idGenerator.genUid());
+    const newExtraState = this.saveExtraState();
+    Blockly.Events.fire(new (Blockly.Events.get(Blockly.Events.BLOCK_CHANGE))(
+      this, 'mutation', null, oldExtraState, newExtraState
+    ));
+    this.updateDisplay();
+  },
+  /**
+   * Click event handler for minus button.
+   */
+  onButtonMinusClick: function() {
+    const oldExtraState = this.saveExtraState();
+    this.argumentids.pop();
+    const newExtraState = this.saveExtraState();
+    Blockly.Events.fire(new (Blockly.Events.get(Blockly.Events.BLOCK_CHANGE))(
+      this, 'mutation', null, oldExtraState, newExtraState
+    ));
+    this.updateDisplay();
+  },
+  /**
+   * Update the block's structure and appearance to match the extra states.
+   */
+  updateDisplay: function() {
+    // Disconnect old blocks, except the label (the first one) and buttons (the last one).
+    const connectionMap: {
+      [key: string]: {
+        shadow: Blockly.serialization.blocks.State | null,
+        block: Blockly.BlockSvg
+      }
+    } = {};
+    for (let i = 1; i < this.inputList.length - 1; ++i) {
+      const input = this.inputList[i];
+      if (input.connection) {
+        const target = input.connection.targetBlock() as Blockly.BlockSvg;
+        connectionMap[input.name] = {
+          shadow: input.connection.getShadowState(),
+          block: target
+        };
+        if (target) {
+          input.connection.disconnect();
+        }
+      }
+    }
 
-//       // populate args
-//       let oldBlock = null;
-//       let oldShadow = null;
-//       if (connectionMap && (id in connectionMap)) {
-//         const saveInfo = connectionMap[id];
-//         oldBlock = saveInfo['block'];
-//         oldShadow = saveInfo['shadow'];
-//       }
+    // Remove all inputs, except the first one and the last one.
+    for (let i = 1; i < this.inputList.length - 1; ++i) {
+      this.inputList[i].dispose();
+    }
+    this.inputList.splice(1, this.inputList.length - 2);
 
-//       if (oldBlock) {
-//         // reattach the old block and shadow dom
-//         connectionMap[input.name] = null;
-//         oldBlock.outputConnection.connect(input.connection);
-//         if (!oldShadow) {
-//           // create shadow dom
-//           oldShadow = goog.dom.createDom('shadow');
-//           oldShadow.setAttribute('type', 'text');
-//           const fieldDom = goog.dom.createDom('field', null, '');
-//           fieldDom.setAttribute('name', 'TEXT');
-//           oldShadow.appendChild(fieldDom);
-//         }
-//         input.connection.setShadowDom(oldShadow);
-//       } else {
-//         // attach shadow
-//         Blockly.Events.disable();
-//         const newBlock = this.workspace.newBlock('text');
-//         newBlock.setFieldValue('', 'TEXT');
-//         newBlock.setShadow(true);
-//         if (wasRendered && !this.isInsertionMarker()) {
-//           newBlock.initSvg();
-//           newBlock.render(false);
-//         }
-//         Blockly.Events.enable();
-//         if (Blockly.Events.isEnabled()) {
-//           Blockly.Events.fire(new Blockly.Events.BlockCreate(newBlock));
-//         }
-//         newBlock.outputConnection.connect(input.connection);
-//       }
-//     }
+    // Create all inputs.
+    for (let i = 0; i < this.argumentids.length; ++i) {
+      const id = this.argumentids[i];
+      const input = this.appendValueInput(id);
 
-//     // delete unused shadow
-//     for (const id in connectionMap) {
-//       const saveInfo = connectionMap[id];
-//       if (saveInfo) {
-//         const block = saveInfo['block'];
-//         if (block && block.isShadow()) {
-//           block.dispose();
-//           connectionMap[id] = null;
-//         }
-//       }
-//     }
+      // Populate argument.
+      let oldBlock = null;
+      let oldShadow = null;
+      if (connectionMap && (id in connectionMap)) {
+        const saveInfo = connectionMap[id];
+        oldBlock = saveInfo.block;
+        oldShadow = saveInfo.shadow;
+      }
 
-//     this.rendered = wasRendered;
-//     if (wasRendered && !this.isInsertionMarker()) {
-//       this.initSvg();
-//       this.render();
-//     }
-//   }
-// };
+      if (oldBlock) {
+        // Reattach the old block and shadow input.
+        delete connectionMap[input.name];
+        oldBlock.outputConnection.connect(input.connection!);
+        if (!oldShadow) {
+          // Create a shadow input.
+          oldShadow = {
+            type: 'text',
+            fields: {TEXT: ''}
+          };
+        }
+        input.connection!.setShadowState(oldShadow);
+      } else {
+        // Create a new shadow block and attach it to the given input.
+        Blockly.Events.disable();
+        let newBlock;
+        try {
+          newBlock = this.workspace.newBlock('text');
+          newBlock.setFieldValue('', 'TEXT');
+          newBlock.setShadow(true);
+          if (!this.isInsertionMarker()) {
+            newBlock.initSvg();
+            newBlock.render();
+          }
+        } finally {
+          Blockly.Events.enable();
+        }
+        if (Blockly.Events.isEnabled()) {
+          Blockly.Events.fire(new (Blockly.Events.get(Blockly.Events.BLOCK_CREATE))(newBlock));
+        }
+        newBlock.outputConnection.connect(input.connection!);
+      }
+    }
+
+    // Delete unused shadow.
+    for (const id in connectionMap) {
+      if (!Object.prototype.hasOwnProperty.call(connectionMap, id)) {
+        continue;
+      }
+      const saveInfo = connectionMap[id];
+      if (saveInfo) {
+        const block = saveInfo['block'];
+        if (block && block.isShadow()) {
+          block.dispose();
+        }
+      }
+    }
+
+    // Move the button input to the end.
+    this.moveInputBefore('DUMMY_INPUT', null);
+
+    // Update the button states.
+    this.buttonMinus.setEnabled(this.argumentids.length > 1);
+  }
+} as OperatorJoinMultipleBlock;
 
 /**
  * Block for 'letter _ of _' operator.
