307aaaa0dfd8692c8522c8ec03cd498cb536b82e
Alex Cui <cuizhihui030925@outlook.com>
2025-11-09 14:37:45 +0800

    :heavy_plus_sign: chore(block): add jest-fixed-jsdom
    Signed-off-by: Alex Cui <cuizhihui030925@outlook.com>

--- packages/block/jest.config.js
+++ packages/block/jest.config.js
@@ -3,21 +3,27 @@ const {createDefaultPreset} = require('ts-jest');
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import('jest').Config} **/
+const sharedConfig = {
+  transform: {
+    ...tsJestTransformCfg
+  },
+  moduleNameMapper: {
+    '\\.(jpg|png|gif|svg|mp3|wav|ogg)$': '<rootDir>/tests/__mocks__/file_mock.js',
+    '\\.(css|less)$': '<rootDir>/tests/__mocks__/style_mock.js'
+  }
+};
+
+/** @type {import('jest').Config} **/
 module.exports = {
   projects: [{
     displayName: 'Unit Tests',
     testMatch: ['<rootDir>/tests/unit/**/*.test.[jt]s'],
     testEnvironment: 'node',
-    transform: {
-      ...tsJestTransformCfg
-    },
-    moduleNameMapper: {
-      '\\.(jpg|png|gif|svg|mp3|wav|ogg)$': '<rootDir>/tests/__mocks__/file_mock.js',
-      '\\.(css|less)$': '<rootDir>/tests/__mocks__/style_mock.js'
-    }
+    ...sharedConfig
   }, {
     displayName: 'DOM Tests',
     testMatch: ['<rootDir>/tests/blocks/**/*.test.[jt]s'],
-    testEnvironment: 'jsdom'
+    testEnvironment: 'jest-fixed-jsdom',
+    ...sharedConfig
   }]
 };
