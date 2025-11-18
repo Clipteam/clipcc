2dc8448dad3df840feee56cd2a9decbe37056f76
SimonShiki <sinangentoo@gmail.com>
2025-11-18 20:55:09 +0800

    :test_tube: test(block): update related tests
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/block/tests/blocks/data.test.ts
+++ packages/block/tests/blocks/data.test.ts
@@ -64,6 +64,7 @@ describe('Blocks: Data', () => {
       const items = Array.from(menu.children).map((element) => element.firstElementChild?.innerHTML);
       expect(items).toStrictEqual([
         Blockly.Msg.DUPLICATE_BLOCK,
+        Blockly.Msg.ADD_COMMENT,
         Blockly.Msg.DELETE_BLOCK,
         'TEST_VARIABLE_2'
       ]);
@@ -159,6 +160,7 @@ describe('Blocks: Data', () => {
       const items = Array.from(menu.children).map((element) => element.firstElementChild?.innerHTML);
       expect(items).toStrictEqual([
         Blockly.Msg.DUPLICATE_BLOCK,
+        Blockly.Msg.ADD_COMMENT,
         Blockly.Msg.DELETE_BLOCK,
         'TEST_LIST_2'
       ]);
