e134b41b23ebdd916e3446d028c61019a33514d8
SimonShiki <sinangentoo@gmail.com>
2025-11-26 20:24:52 +0800

    :rewind: revert: ":bug: fix: legacy dependencies to make gui cannot run"
    This reverts commit 872688fabac4da213d59e3d925c2d839644064df.

--- packages/gui/webpack.config.js
+++ packages/gui/webpack.config.js
@@ -8,6 +8,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const STATIC_PATH = process.env.STATIC_PATH || '/static';
 
@@ -284,6 +285,10 @@ module.exports = [
                             to: 'libraries/[name][ext]'
                         }
                     ]
+                }),
+                new WorkboxPlugin.GenerateSW({
+                    clientsClaim: true,
+                    skipWaiting: true
                 })
             ])
         })) : []
