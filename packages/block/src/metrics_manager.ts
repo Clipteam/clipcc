3423b66f159941059e15e28af5a985a42a7e274a
SimonShiki <sinangentoo@gmail.com>
2025-12-02 18:48:15 +0800

    :wrench: chore(block): don't use continuous-toolbox
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/block/src/metrics_manager.ts
+++ packages/block/src/metrics_manager.ts
@@ -7,9 +7,8 @@
 import * as Blockly from 'blockly/core';
 import {getCommentBubbleFromBlock} from './utils';
 import type {BlockCommentDelete} from './events/block_comment_delete';
-import {ContinuousMetrics} from '@blockly/continuous-toolbox';
 
-export class MetricsManager extends ContinuousMetrics {
+export class MetricsManager extends Blockly.MetricsManager {
   protected trackedCommentedBlocks = new Set<Blockly.Block>();
 
   constructor(workspace: Blockly.WorkspaceSvg) {
@@ -92,6 +91,77 @@ export class MetricsManager extends ContinuousMetrics {
     }
 
     return metrics;
+  }
+
+  /**
+   * Computes the viewport size to not include the toolbox and the flyout.
+   * The default viewport includes the flyout.
+   * @param getWorkspaceCoordinates True to get the view metrics in workspace
+   *     coordinates, false to get them in pixel coordinates.
+   * @returns The width, height, top and left of the viewport in either
+   *     workspace coordinates or pixel coordinates.
+   */
+  override getViewMetrics(
+    getWorkspaceCoordinates = false
+  ): Blockly.MetricsManager.ContainerRegion {
+    const scale = getWorkspaceCoordinates ? this.workspace_.scale : 1;
+    const svgMetrics = this.getSvgMetrics();
+    const toolboxMetrics = this.getToolboxMetrics();
+    const flyoutMetrics = this.getFlyoutMetrics(false);
+    const toolboxPosition = toolboxMetrics.position;
+
+    if (this.workspace_.getToolbox()) {
+      // Note: Not actually supported at this time due to ContinuousToolbox
+      // only supporting a vertical flyout. But included for completeness.
+      if (
+        toolboxPosition == Blockly.TOOLBOX_AT_TOP ||
+        toolboxPosition == Blockly.TOOLBOX_AT_BOTTOM
+      ) {
+        svgMetrics.height -= toolboxMetrics.height + flyoutMetrics.height;
+      } else if (
+        toolboxPosition == Blockly.TOOLBOX_AT_LEFT ||
+        toolboxPosition == Blockly.TOOLBOX_AT_RIGHT
+      ) {
+        svgMetrics.width -= toolboxMetrics.width + flyoutMetrics.width;
+      }
+    }
+    return {
+      height: svgMetrics.height / scale,
+      width: svgMetrics.width / scale,
+      top: -this.workspace_.scrollY / scale,
+      left: -this.workspace_.scrollX / scale
+    };
+  }
+
+  /**
+   * Gets the absolute left and absolute top in pixel coordinates.
+   * This is where the visible workspace starts in relation to the SVG
+   * container, adjusted to not include the area behind the flyout.
+   * @returns The absolute metrics for the workspace.
+   */
+  override getAbsoluteMetrics(): Blockly.MetricsManager.AbsoluteMetrics {
+    const toolboxMetrics = this.getToolboxMetrics();
+    const flyoutMetrics = this.getFlyoutMetrics(false);
+    const toolboxPosition = toolboxMetrics.position;
+    let absoluteLeft = 0;
+
+    if (
+      this.workspace_.getToolbox() &&
+      toolboxPosition == Blockly.TOOLBOX_AT_LEFT
+    ) {
+      absoluteLeft = toolboxMetrics.width + flyoutMetrics.width;
+    }
+    let absoluteTop = 0;
+    if (
+      this.workspace_.getToolbox() &&
+      toolboxPosition == Blockly.TOOLBOX_AT_TOP
+    ) {
+      absoluteTop = toolboxMetrics.height + flyoutMetrics.height;
+    }
+    return {
+      top: absoluteTop,
+      left: absoluteLeft
+    };
   }
 }
 
