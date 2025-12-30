58f15c4270931ef887e2b98778890c216ab03cfe
SimonShiki <sinangentoo@gmail.com>
2025-12-30 17:16:31 +0800

    :wrench: chore(storage): remove unused jsdoc
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/storage/src/AssetType.ts
+++ packages/storage/src/AssetType.ts
@@ -9,13 +9,6 @@ export interface IAssetType {
 
 /**
  * Enumeration of the supported asset types.
- * @type {Record<string, IAssetType>}
- * @typedef {object} IAssetType - Information about a supported asset type.
- * @property {string} contentType - the MIME type associated with this kind of data. Useful for data URIs, etc.
- * @property {string} name - The human-readable name of this asset type.
- * @property {IDataFormat} runtimeFormat - The default format used for runtime, in-memory storage of this asset. For
- *     example, a project stored in SB2 format on disk will be returned as JSON when loaded into memory.
- * @property {boolean} immutable - Indicates if the asset id is determined by the asset content.
  */
 export const AssetType = {
     ImageBitmap: {
