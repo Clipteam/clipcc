fb86517595398228b575affc5f9b020d2a087479
SimonShiki <sinangentoo@gmail.com>
2026-02-27 17:36:11 +0800

    :bug: fix(gui): unexpected procedure editor focus
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/gui/src/containers/custom-procedures.jsx
+++ packages/gui/src/containers/custom-procedures.jsx
@@ -152,8 +152,6 @@ class CustomProcedures extends React.Component {
         if (this.mutationRoot) {
             const newReturn = !this.mutationRoot.getReturn();
             this.mutationRoot.setReturn(newReturn);
-            this.mutationRoot.updateDisplay_();
-            this.mutationRoot.focusLastEditor_();
             this.setState({return: newReturn});
         }
     }
