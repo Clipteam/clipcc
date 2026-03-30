a529775cf6dc6fe6b0a7110ed603da1d2ecc7ed8
Simon Shiki <sinangentoo@gmail.com>
2026-03-30 16:41:23 +0800

    :bug: fix(block): make blockly's copy & cut shortcut match scratch's behavior (#207)
    * :bug: fix(block): make blockly's copy & paste shortcut match scratch's behavior
    
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>
    
    * :wrench: chore: add shortcut_items.ts
    
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>
    
    ---------
    
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/block/src/index.ts
+++ packages/block/src/index.ts
@@ -9,6 +9,7 @@ import * as Blockly from 'blockly/core';
 import * as Constants from './constants';
 import {injectCssVariables, Scratch} from './theme';
 import {registerScratchContextMenu} from './contextmenu_items';
+import {registerScratchShortcuts} from './shortcut_items';
 import {FieldAngle, registerFieldAngle} from './fields/angle';
 import {FieldButton, registerFieldButton} from './fields/button';
 import {FieldColourSlider, registerFieldColourSlider} from './fields/colour_slider';
@@ -198,6 +199,7 @@ function setupEnvironment() {
   registerFieldVariableGetter();
   registerFieldVerticalSeparator();
   registerScratchContextMenu();
+  registerScratchShortcuts();
 
   Blockly.Css.register(styles);
   Blockly.Css.register(commentStyles);
