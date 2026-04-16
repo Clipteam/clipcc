52dc023fbe838388eb6bd5eea00882111bddb030
Simon Shiki <sinangentoo@gmail.com>
2026-04-16 18:13:33 +0800

    :wrench: chore: migrate libraryTarget (#222)

--- packages/l10n/webpack.config.js
+++ packages/l10n/webpack.config.js
@@ -28,6 +28,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        libraryTarget: 'commonjs2'
+        library: {
+            type: 'commonjs2'
+        }
     }
 };
