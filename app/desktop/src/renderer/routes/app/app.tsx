cf5d530805f63316b82ad00eccc5377050a6e055
SimonShiki <sinangentoo@gmail.com>
2026-04-18 08:39:09 +0800

    :wrench: chore(desktop): remove rebundant type inferrence
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- app/desktop/src/renderer/routes/app/app.tsx
+++ app/desktop/src/renderer/routes/app/app.tsx
@@ -12,12 +12,10 @@ appTarget.classList.add(styles.app);
 
 setAppElement(appTarget);
 
-type PropsOf<C> = C extends React.ComponentType<infer P> ? P : never;
-
 // note that redux's 'compose' function is just being used as a general utility to make
 // the hierarchy of HOC constructor calls clearer here; it has nothing to do with redux's
 // ability to compose reducers.
-const WrappedGui = compose<React.ComponentType<PropsOf<typeof GUI>>>(
+const WrappedGui = compose<typeof GUI>(
     ScratchDesktopAppStateHOC,
     AppStateHOC,
     ScratchDesktopGUIHOC
