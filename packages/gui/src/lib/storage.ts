50e0153d6ee5aba78b200f13481c400387d516d4
Simon Shiki <sinangentoo@gmail.com>
2026-08-16 16:09:34 +0800

    :wrench: chore(gui): refine gui types (#239)
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- packages/gui/src/lib/storage.ts
+++ packages/gui/src/lib/storage.ts
@@ -1,36 +1,16 @@
 import {
     ScratchStorage,
-    type Asset,
-    type AssetData,
-    type AssetId
+    type Asset
 } from 'clipcc-storage';
 
 import defaultProject from './default-project';
 
-type Translator = ((messageId: string, defaultMessage?: string, description?: string) => string) | undefined;
+export type Translator = ((messageId: string, defaultMessage?: string, description?: string) => string) | undefined;
 
 type ConfigResponse = string | {
     url: string;
     withCredentials: boolean;
     method?: 'post';
-};
-
-interface DefaultProjectAsset {
-    id: AssetId;
-    assetType: string;
-    dataFormat: string;
-    data: AssetData;
-}
-
-const isRecord = (value: unknown): value is Record<string, unknown> =>
-    typeof value === 'object' && value !== null;
-
-const isDefaultProjectAsset = (value: unknown): value is DefaultProjectAsset => {
-    if (!isRecord(value)) return false;
-    return typeof value.id !== 'undefined' &&
-        typeof value.assetType === 'string' &&
-        typeof value.dataFormat === 'string' &&
-        typeof value.data !== 'undefined';
 };
 
 /**
@@ -48,7 +28,7 @@ class Storage extends ScratchStorage {
         this.cacheDefaultProject();
     }
 
-    addOfficialScratchWebStores (): void {
+    addOfficialScratchWebStores () {
         this.addWebStore(
             [this.AssetType.Project],
             this.getProjectGetConfig.bind(this),
@@ -70,11 +50,11 @@ class Storage extends ScratchStorage {
         );
     }
 
-    setProjectHost (projectHost: string): void {
+    setProjectHost (projectHost: string) {
         this.projectHost = projectHost;
     }
 
-    setProjectToken (projectToken: string): void {
+    setProjectToken (projectToken: string) {
         this.projectToken = projectToken;
     }
 
@@ -98,7 +78,7 @@ class Storage extends ScratchStorage {
         };
     }
 
-    setAssetHost (assetHost: string): void {
+    setAssetHost (assetHost: string) {
         this.assetHost = assetHost;
     }
 
@@ -118,19 +98,15 @@ class Storage extends ScratchStorage {
         };
     }
 
-    setTranslatorFunction (translator: Translator): void {
+    setTranslatorFunction (translator: Translator) {
         this.translator = translator;
         this.cacheDefaultProject();
     }
 
-    cacheDefaultProject (): void {
+    cacheDefaultProject () {
         const defaultProjectAssets = defaultProject(this.translator);
 
         defaultProjectAssets.forEach(asset => {
-            if (!isDefaultProjectAsset(asset)) {
-                return;
-            }
-
             const assetType = this.AssetType[asset.assetType as keyof typeof this.AssetType];
             const dataFormat = this.DataFormat[asset.dataFormat as keyof typeof this.DataFormat];
             if (!assetType || !dataFormat) {
